064f1acbc1c567ecbf70648eff8008fca59edc2f
lg <abbix@riseup.net>
2022-12-05 20:32:03 -0500

    Add flags to qemu

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ARCH="x86_64"
 MEMORY="2G"
-cmd="qemu-system-${ARCH} -enable-kvm -m ${MEMORY} -cdrom nyx.iso"
+cmd="qemu-system-${ARCH} -enable-kvm -m ${MEMORY} -cdrom nyx.iso -debugcon stdio -cpu host"
 
 ./install-limine.sh
 
