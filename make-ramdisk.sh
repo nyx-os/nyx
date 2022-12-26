9fc596874091eeca69c33dfce1f1f049e234626a
lg <abbix@riseup.net>
2022-12-25 19:26:55 -0500

    Build ramdisk

---
+++ make-ramdisk.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd boot
+tar -cf ramdisk.tar *
+cd ..
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ ARCH="x86_64"
 MEMORY="2G"
 cmd="qemu-system-${ARCH} -enable-kvm -m ${MEMORY} -cdrom nyx.iso -debugcon stdio -cpu host"
 
+./make-ramdisk.sh
 ./install-limine.sh
 
 $cmd
