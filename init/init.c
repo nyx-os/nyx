148a23acf22bf56f2c568fe6ec4f3952aeb536f3
lg <abbix@riseup.net>
2023-05-14 10:14:49 -0400

    feat: add fork() and exec

--- init/init.c
+++ init/init.c
@@ -12,24 +12,19 @@ int main(void) {
     return EXIT_FAILURE;
   }
 
-  while (true) {
-    printf("Welcome to \033[1;32mnyx\033[0m, your shell is %s\n", shell);
+  printf("Welcome to \033[1;32mnyx\033[0m, your shell is %s\n", shell);
 
-    int pid = fork();
+  int pid = fork();
 
-    if (pid == -1) {
-      perror("fork");
-      return EXIT_FAILURE;
-    }
+  if (pid == -1) {
+    perror("fork");
+    return EXIT_FAILURE;
+  }
 
-    if (pid == 0) {
-      char *argv[] = {shell, NULL};
-      execvp(shell, argv);
-      return EXIT_FAILURE;
-    } else {
-      int status;
-      waitpid(pid, &status, 0);
-    }
+  if (pid == 0) {
+    char *argv[] = {shell, NULL};
+    execvp(shell, argv);
+    return EXIT_FAILURE;
   }
 
   return 0;
