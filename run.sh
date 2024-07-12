7859c01cc2f2680911a3124bdc6151cb5a6dffa4
lg <abbix@riseup.net>
2024-07-12 19:36:42 -0400

    remove extra files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ARCH="x86_64"
-MEMORY="2G"
-cmd="qemu-system-${ARCH} -enable-kvm -m ${MEMORY} -cdrom nyx.iso -debugcon stdio -cpu host -no-shutdown -no-reboot"
-
-./make-ramdisk.sh
-./install-limine.sh
-
-$cmd
